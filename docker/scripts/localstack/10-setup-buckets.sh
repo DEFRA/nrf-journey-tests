03c2be2c97225d1948ad665f3eebfee04d2d5148
Jonny Wyatt <jonnywyatt@yahoo.co.uk>
2026-04-27 09:26:00 +0100

    Various fixes to SQS queue / seed data / config to get email send test working

--- docker/scripts/localstack/10-setup-buckets.sh
+++ docker/scripts/localstack/10-setup-buckets.sh
@@ -2,9 +2,23 @@
 
 # Create LocalStack resources needed by the NRF stack
 
-# SNS topic for quote estimate requests (used by nrf-backend)
+# SNS topic for quote estimate requests (published by nrf-backend,
+# consumed by nrf-impact-assessor via the SQS subscription below).
 awslocal sns create-topic \
-  --name nrf_quote_estimate_request \
+  --name nrf-quote-estimate-request \
+  --region eu-west-2
+
+# SQS queue that nrf-impact-assessor polls for assessment jobs.
+awslocal sqs create-queue \
+  --queue-name nrf-impact-assessment-jobs \
+  --region eu-west-2
+
+# Subscribe the assessor's queue to the quote-estimate SNS topic so messages
+# published by nrf-backend are delivered to nrf-impact-assessor.
+awslocal sns subscribe \
+  --topic-arn arn:aws:sns:eu-west-2:000000000000:nrf-quote-estimate-request \
+  --protocol sqs \
+  --notification-endpoint arn:aws:sqs:eu-west-2:000000000000:nrf-impact-assessment-jobs \
   --region eu-west-2
 
 # S3 buckets for file uploads (used by cdp-uploader)
